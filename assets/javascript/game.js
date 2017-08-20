8bd52cfe433de125233ff83812a23b3c4dc3b346
morgan greenwalt <you@example.com>
2017-08-20 14:29:49 -0500

    fixing crystal game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,103 +1,107 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-//Define Crystal Variables
-var lowEndCrystal = 1;
-var highEndCrystal = 12;
-var crystalValues = [];
-while(lowEndCrystal <= highEndCrystal){
-   crystalValues.push(lowEndCrystal++);
-}
-//var crystalNumber = crystalValues[Math.floor(Math.random()* crystalValues.length)];
+    //Define Crystal Variables
+    var lowEndCrystal = 1;
+    var highEndCrystal = 12;
+    var crystalValues = [];
+    while (lowEndCrystal <= highEndCrystal) {
+        crystalValues.push(lowEndCrystal++);
+    }
 
-//Define Computer Score 
-var lowEndComputer = 19;
-var highEndComputer = 120;
-var computerValues = [];
-while(lowEndComputer <= highEndComputer){
-   computerValues.push(lowEndComputer++);
-}
-var computerScore = computerValues[Math.floor(Math.random()* computerValues.length)];
-$("#computer-score-value").append(computerScore);
+    //Define Computer Score 
+    var lowEndComputer = 19;
+    var highEndComputer = 120;
+    var computerValues = [];
+    while (lowEndComputer <= highEndComputer) {
+        computerValues.push(lowEndComputer++);
+    }
 
-//Define Player Score
-var playerScore = 0;
-$("#player-score-value").html(playerScore);
+    //Define All Other Variables
+    var wins = 0;
+    var losses = 0;
+    var playerScore = 0;
+    var computerScore;
+    var crystalsObject;
 
-//Define Wins and Losses
-var wins = 0;
-$(".wins").html(wins);
-var losses = 0;
-$(".losses").html(losses);
+    // jQuery write to page
+    $(".wins").html(wins);
+    $(".losses").html(losses);
+    $("#playerScore").html(playerScore);
+    $("#computerScore").html(computerScore);
 
-//Assign Random Values to Crystals
-var firstCrystal = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-var secondCrystal = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-var thirdCrystal = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-var fourthCrystal = crystalValues[Math.floor(Math.random()* crystalValues.length)];
+    // Start Game 
+    startAndReset();
 
-//Testing
-console.log(firstCrystal);
-console.log(secondCrystal);
-console.log(thirdCrystal);
-console.log(fourthCrystal);
+    // Generate Random Crystal Values
+    function random() {
+        return Math.floor((Math.random() * crystalValues.length) + 1);
+    }
 
-//Click Events Assigning Values to Crystals
-$(".crystal-1").on("click", function (){
-	$(".crystal-number-1").hide(firstCrystal);
-	playerScore = playerScore + firstCrystal;
-	$("#player-score-value").html(playerScore);
-	check();
-});
+    // Generate Random Computer Score
+    function randomComp() {
+        return Math.floor((Math.random() * computerValues.length) + 1);
+    }
 
-$(".crystal-2").on("click", function (){
-	$(".crystal-number-2").hide(secondCrystal);
-	playerScore = playerScore + secondCrystal;
-	$("#player-score-value").html(playerScore);
-	check();
-});
+    // Crystals Object w/ Random Values
+    function crystals() {
+        crystalsObject = {
+            crystal1: random(),
+            crystal2: random(),
+            crystal3: random(),
+            crystal4: random()
+        };
+    }
 
-$(".crystal-3").on("click", function (){
-	$(".crystal-number-3").hide(thirdCrystal);
-	playerScore = playerScore + thirdCrystal;
-	$("#player-score-value").html(playerScore);
-	check();
-});
+    //Click Events for Crystals + Game Play
+    $(".crystal1").on("click", function() {
+        $(".crystalScore1").hide(crystalsObject.crystal1);
+        playerScore += crystalsObject.crystal1;
+        $("#playerScore").html(playerScore);
+        checkScores();
+    });
 
-$(".crystal-4").on("click", function (){
-	$(".crystal-number-4").hide(fourthCrystal);
-	playerScore = playerScore + fourthCrystal;
-	$("#player-score-value").html(playerScore);
-	check();
-});
+    $(".crystal2").on("click", function() {
+        $(".crystalScore2").hide(crystalsObject.crystal2);
+        playerScore += crystalsObject.crystal2;
+        $("#playerScore").html(playerScore);
+        checkScores();
+    });
 
-//Display Wins & Losses
+    $(".crystal3").on("click", function() {
+        $(".crystalScore3").hide(crystalsObject.crystal3);
+        playerScore += crystalsObject.crystal3;
+        $("#playerScore").html(playerScore);
+        checkScores();
+    });
 
-function check() {
-	if (playerScore === computerScore) {
-		//alert("You win!");
-		wins++;
-		$(".wins").html(wins);
-		resetGame();
+    $(".crystal4").on("click", function() {
+        $(".crystalScore4").hide(crystalsObject.crystal4);
+        playerScore += crystalsObject.crystal4;
+        $("#playerScore").html(playerScore);
+        checkScores();
+    });
 
-	}
+    //Check Player vs. Computer Score & Display Wins & Losses
+    function checkScores() {
+        if (playerScore === computerScore) {
+            wins++;
+            $(".wins").html(wins);
+            startAndReset();
 
-	else if (playerScore > computerScore){
-		//alert("You went over! Try again.")
-		losses++;
-		$(".losses").html(losses);
-		resetGame();
-	}
+        } else if (playerScore > computerScore) {
+            losses++;
+            $(".losses").html(losses);
+            startAndReset();
+        }
+    }
 
-}
-	function resetGame (){
-		playerScore = 0;
-		$("#player-score-value").html(playerScore);
-		computerScore = computerValues[Math.floor(Math.random()* computerValues.length)];
-		$("#computer-score-value").html(computerScore);
-		firstCrystal = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-		secondCrystal = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-		thirdCrystal = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-		fourthCrystal = crystalValues[Math.floor(Math.random()* crystalValues.length)];
-}
-
+    // Start & Reset Game
+    function startAndReset() {
+        random();
+        crystals();
+        playerScore = 0;
+        $("#playerScore").html(playerScore);
+        computerScore = randomComp();
+        $("#computerScore").html(computerScore);
+    }
 });
